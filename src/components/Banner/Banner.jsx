cec9f3ecff7968ba41cebb6f1095212b2eecb0ec
mkasiam <kawsarahmmed10001@gmail.com>
2023-09-25 23:05:19 +0600

    Banner completed and fake data for categories is also done

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+const Banner = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-7 md:my-10 lg:my-16 space-y-4 md:space-y-5 lg:space-y-8">
+      <h1 className="text-[#0B0B0B] text-2xl md:text-3xl lg:text-4xl font-bold text-center">
+        I Grow By Helping People In Need
+      </h1>
+      <div>
+          <div className="input-group rounded-none">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered rounded-sm "
+            />
+            <button className="btn px-2 rounded-sm">Search</button>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import Banner from "../Banner/Banner";
 
 const Header = () => {
     const activeStyle = ({ isActive }) => ({
@@ -7,32 +8,35 @@ const Header = () => {
       })
   return (
     <>
-      <div className="flex justify-between items-center">
-        <div>
-          <img
-            className="w-2xl"
-            src="https://i.ibb.co/jZHBKsy/Logo.png"
-            alt=""
-          />
+      <div>
+        <div className="flex flex-col md:flex-row lg:flex-row justify-between items-center">
+            <div>
+            <img
+                className="w-2xl"
+                src="https://i.ibb.co/jZHBKsy/Logo.png"
+                alt=""
+            />
+            </div>
+            <div>
+            <ul className="flex text-xl font-medium">
+                <li className="px-4">
+                <NavLink
+                    to="/"
+                    style={activeStyle}
+                >
+                    Home
+                </NavLink>
+                </li>
+                <li className="px-4">
+                <NavLink to="/donation" style={activeStyle}>Donation</NavLink>
+                </li>
+                <li className="px-4">
+                <NavLink to="/statistics" style={activeStyle}>Statistics</NavLink>
+                </li>
+            </ul>
+            </div>
         </div>
-        <div>
-          <ul className="flex text-xl font-medium border border-black">
-            <li className="px-4">
-              <NavLink
-                to="/"
-                style={activeStyle}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="px-4">
-              <NavLink to="/donation" style={activeStyle}>Donation</NavLink>
-            </li>
-            <li className="px-4">
-              <NavLink to="/statistics" style={activeStyle}>Statistics</NavLink>
-            </li>
-          </ul>
-        </div>
+        <Banner></Banner>
       </div>
     </>
   );
