36c288a0aeb7ea21d5a86fc15609165eda3deba0
mkasiam <kawsarahmmed10001@gmail.com>
2023-09-26 16:16:28 +0600

    working with json data

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Category = ({category}) => {
-    const {id,img,category_name,title}=category;
+    const {id,img,card_color,category_name,title,card_common_color}=category;
     
     return (
         <div>
             <Link to={`/donation/${id}`}>
-                <div>
-                    <div className="border border-black rounded-md">
+                <div className={`rounded-md`}>
+                    <div className=" rounded-md">
                         <img className="w-full rounded-md" src={img} alt="No image found" />
                     </div>
-                    <div>
-                        <p className="p-2 bg-slate-300 max-w-fit rounded-md">{category_name}</p>
+                    <div className="py-4 space-y-2">
+                        <p className={`p-2 bg-slate-300 max-w-fit rounded-md`}>{category_name}</p>
                         <h1 className="text-xl">{title}</h1>
                     </div>
                 </div>
